7df39cbe2bf4bc37e33c7651e5cf42a63aa54f69
Wojciech Wesołowski <wojtek.wes@gmail.com>
2020-09-06 15:01:13 +0200

    Implement list questions

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+headers = ['id','submission_time','view_number','vote_number','title','message','image']
+questions = [
+    {
+        'id': 1,
+        'submission': 1493368154,
+        'view_number': 29,
+        'vote_number': 7,
+        'title': "How to make lists in Python?",
+        'message': "I am totally new to this, any hints?",
+        'image': ''
+    }
+]
+
+
+@app.route('/list')
+def list():
+    return render_template('index.html', headers=headers, questions=questions)
+
+
+if __name__ == '__main__':
+    app.run()
